2ae16eb548dd988530cfb32af236645732e0561d
N1shko <flages1325@gmail.com>
2023-03-29 22:44:14 +0300

    added DB dump files

---
+++ Dump20230315/123_blank.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: 123
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blank`
+--
+
+DROP TABLE IF EXISTS `blank`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `blank` (
+  `blank_id` int NOT NULL AUTO_INCREMENT,
+  `blank_last_update` date NOT NULL,
+  `blank_amount` int NOT NULL,
+  `blank_price` int NOT NULL,
+  `blank_nomen_id` int NOT NULL,
+  PRIMARY KEY (`blank_id`),
+  UNIQUE KEY `blank_id_UNIQUE` (`blank_id`),
+  KEY `blank_nomen_id_idx` (`blank_nomen_id`),
+  CONSTRAINT `blank_nomen_id` FOREIGN KEY (`blank_nomen_id`) REFERENCES `nomenklatura` (`Nomen_blank_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blank`
+--
+
+LOCK TABLES `blank` WRITE;
+/*!40000 ALTER TABLE `blank` DISABLE KEYS */;
+INSERT INTO `blank` VALUES (26,'2019-12-12',8,50,5),(27,'2020-03-02',3,100,3),(28,'2020-03-18',2,25,4),(29,'2020-03-18',1,50,4),(30,'2020-08-24',2,150,1),(31,'2020-09-13',6,50,2),(32,'2022-12-09',5730,100,1),(33,'2020-03-24',1,300,6),(34,'2022-12-05',304,200,4),(35,'2022-12-07',1809,200,1),(36,'2022-11-19',43,120,2),(37,'2022-11-23',433,120,1),(38,'2022-11-23',853,200,2),(39,'2022-11-19',23,12,1),(40,'2022-11-19',200,32,1),(41,'2022-11-19',120,32,4),(42,'2022-12-03',10,300,1),(43,'2022-12-07',800,300,2),(44,'2022-12-09',400,300,5);
+/*!40000 ALTER TABLE `blank` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-15 23:27:31
